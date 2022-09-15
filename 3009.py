8ee33c0c2e710b1239d90d34b5455ef7d5588a80
thisishwan2 <feel000617@gmail.com>
2022-09-15 14:54:40 +0900

    first commit

--- 3009.py
+++ 3009.py
@@ -1,4 +1,4 @@
-a1,b1=map(int, input().split())
+'''a1,b1=map(int, input().split())
 a2,b2=map(int, input().split())
 a3,b3=map(int, input().split())
 
@@ -16,4 +16,20 @@ elif b1==b3:
 else:
     b4=b1
 
-print(a4,b4)
+print(a4,b4)'''
+
+#리스트를 이용해 간략히 풀면
+
+x_num=[]
+y_num=[]
+for _ in range(3):
+    x,y=map(int,input().split())
+    x_num.append(x)
+    y_num.append(y)
+
+for i in range(3):
+    if x_num.count(x_num[i])==1:
+        x4=x_num[i]
+    if y_num.count(y_num[i])==1:
+        y4=y_num[i]
+print(x4,y4)
