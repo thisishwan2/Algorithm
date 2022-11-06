bb16f85cd7e65c1c1b70663b2b8dc03ab7adc168
Hawn <feel000617@gmail.com>
2022-11-06 16:09:20 +0900

    그리디+정렬+수학

--- python/그리디/2217.py
+++ python/그리디/2217.py
@@ -12,6 +12,9 @@ for _ in range(n):
 weight_lst.sort(reverse=True)
 cnt=1
 maxi=0
+
+#최대 중량 구하는 for문. 무거운 무게부터 1개씩 늘려나감
+#ex) w=20 -> maxi=20, w=15,20 -> maxi=30 w=20일때랑 w=15, 20일때 비교 후 더 큰수 저장.
 for i in weight_lst:
     a=i*cnt
     maxi=max(maxi,a)
