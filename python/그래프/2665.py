ba57e3201bb0bf2552b272d48fa1ff2ed6b5c984
Hawn <feel000617@gmail.com>
2022-12-21 01:31:09 +0900

    BFS+다익스트라

--- python/그래프/2665.py
+++ python/그래프/2665.py
@@ -80,3 +80,55 @@ def dijkstra(x,y):
                     heapq.heappush(hq, [dist+1, nx, ny])
                     
 dijkstra(0,0)
+
+'''
+내가 푼 풀이(정답)
+import sys
+input=sys.stdin.readline
+from collections import deque
+
+
+n=int(input())
+
+room=[]
+for i in range(n):
+    room.append(list(map(int,input().rstrip())))
+
+distance=[[0]*(n) for _ in range(n)]
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+def bfs(x,y):
+    q=deque()
+    q.append([x,y])
+    distance[x][y]=1
+
+    while q:
+        x,y=q.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+
+            if 0<=nx<n and 0<=ny<n:
+                if distance[nx][ny]==0:
+                    if room[nx][ny]==1:
+                        distance[nx][ny]=distance[x][y]
+                    if room[nx][ny]==0:
+                        distance[nx][ny]=distance[x][y]+1
+                    q.append([nx,ny])
+
+                elif distance[nx][ny]!=0:
+                    if room[nx][ny]==1:
+                        if distance[nx][ny]>distance[x][y]:
+                            q.append([nx,ny])
+                            distance[nx][ny]=distance[x][y]
+                    if room[nx][ny]==0:
+                        if distance[nx][ny]>distance[x][y]+1:
+                            distance[nx][ny]=distance[x][y]+1
+                            q.append([nx,ny])
+
+bfs(0,0)
+print(distance[n-1][n-1]-1)
+
+'''
