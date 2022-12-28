bdd6feec9f3f5dcb895eb535dcda5c1883d80329
Hawn <feel000617@gmail.com>
2022-12-28 20:31:07 +0900

    BFS

--- python/그래프/12851.py
+++ python/그래프/12851.py
@@ -26,7 +26,7 @@ def bfs(x,times):
         for i in (x-1,x+1,2*x):
             nx=i
             if 0<=nx<=100000:
-                if visit[nx]==0 or visit[nx]==visit[x]+1: #뒷조건을 생각하지를 못했음..
+                if visit[nx]==0 or visit[nx]==visit[x]+1: #뒷조건을 생각하지를 못했음.. 같은경우만 생각해도 되는 이유는 BFS는 어차피 최단 거리로 도달지점을 달성하므로, 그 이하로 시간이 걸리지 않기 때문
                     q.append([nx,times+1])
                     visit[nx]=visit[x]+1
     return (min(res), cnt)
