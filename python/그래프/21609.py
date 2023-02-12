69f9f395868ba1d0a58dfe210aecd9d1cfb14e09
Hawn <feel000617@gmail.com>
2023-02-12 15:15:24 +0900

    BFS+시뮬레이션

--- python/그래프/21609.py
+++ python/그래프/21609.py
@@ -11,6 +11,80 @@
 # 4. 90반시계 회전
 # 5. 격자 중력작용
 
+#가장큰 블록그룹 찾기
+def bfs(x,y,color):
+    q=deque()
+    q.append([x,y])
+    visited[x][y]=1
+
+    #기준 블럭
+    standard_block_x=x
+    standard_block_y=y
+
+    total_cnt, rainbow =1,0 #전체 블록수, 무지개 블럭 수
+    normal, rainbow_lst = [[x,y]],[] #일반 블럭, 무지개 블럭 좌표 리스트
+
+    while q:
+        x,y=q.popleft()
+        for k in range(4):
+            nx=x+dx[k]
+            ny=y+dy[k]
+            if 0<=nx<n and 0<=ny<n:
+                # 아직 방문하지 않았고, 일반블럭일때
+                if visited[nx][ny]==0 and graph[nx][ny]==color:
+                    q.append([nx,ny])
+                    visited[nx][ny]=1
+                    normal.append([nx,ny])
+                    total_cnt+=1
+                # 아직 빙문하지 않았고, 무지개 블럭일때
+                if visited[nx][ny]==0 and graph[nx][ny]==0:
+                    q.append([nx,ny])
+                    visited[nx][ny]=1
+                    rainbow_lst.append([nx,ny])
+                    total_cnt+=1
+                    rainbow+=1
+    
+    # 무지개의 방문처리는 초기화(이후 탐색에서 중복으로 사용될 수 있음.)
+    for x,y in rainbow_lst:
+        visited[x][y]=0
+
+    return total_cnt, rainbow, standard_block_x, standard_block_y, normal+rainbow_lst # 총 갯수, 무지개 갯수, 기준블럭, 해당 블럭들의 좌표
+
+# 아랫 방향으로 어디까지 갈 수 있는지 탐색
+def gravity_bfs(x,y):
+    q=deque()
+    q.append([x,y])
+
+    while q:
+        x,y=q.popleft()
+        nx=x+1
+        #범위내에 다음이 빈칸이면,
+        if 0<=nx<n and graph[nx][y]==-2:
+            q.append([nx,y])
+    #갈수있는 제일 아래의 x,y를 반환
+    return x,y
+
+#중력작용(검은색 블록 제외 아래로 이동)
+def gravity():
+    for i in range(n-2,-1,-1): #밑의 한줄 위부터 체크한다.(맨 밑줄은 내려갈 곳이 없음.)
+        for j in range(n):
+            #블럭이 일반 혹은 무지개 블럭이면
+            if graph[i][j]>=0 :
+                x,y=gravity_bfs(i,j)
+                graph[x][y]=graph[i][j] #이동
+                #만약 처음좌표와 탐색결과의 좌표가 같지 않다면,
+                if (x,y)!=(i,j):
+                    #처음 자리는 빈칸으로 변경
+                    graph[i][j]=-2
+
+#회전
+def turn():
+    turn_graph=[[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            turn_graph[n-j-1][i]=graph[i][j]
+    return turn_graph
+
 
 import sys
 input=sys.stdin.readline
@@ -24,121 +98,42 @@ for _ in range(n):
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-#가장큰 블록그룹 찾기(일반 블록이 하나 이상있고, 일반 블록의 색은 동일해야함., 검은색 블록은 포함 불가. 무지개 블록은 상관없다. 그룹에는 2개 이상의 블록이 있어야함., 인접해야함.)
-def bfs(x,y):
-    i,j=x,y
-    q=deque()
-    q.append([x,y])
-    visited[x][y]=1
-    total_cnt=1 #전체 블록수
-    coordinate=[] #블록그룹 좌표 담을 리스트
-    coordinate.append([x,y])
-
-    rainbow_lst=[]
-
-    #기준 블럭(기준 블럭은 무지개 블럭일 수 없다.)
-    if graph[x][y]!=0:
-        gizoon_x=x
-        gizoon_y=y
-    else:
-        gizoon_x,gizoon_y=sys.maxsize,sys.maxsize
-
-    #시작이 무지개 블럭이면, 무지개 블럭 개수를 1로 시작하고, 리스트에 넣는다.
-    if graph[x][y]==0:
-        rainbow=1
-        rainbow_lst.append([x,y])
-    else:
-        rainbow=0
-
-    while q:
-        x,y=q.popleft()
-        for k in range(4):
-            nx=x+dx[k]
-            ny=y+dy[k]
-            if 0<=nx<n and 0<=ny<n:
-                # 아직 방문하지 않았고, 검은색블럭, 빈칸이 아닐때
-                if visited[nx][ny]==0 and graph[nx][ny]!=-1 and graph[nx][ny]!=-2:
-                    # 그래프가 무지개블럭 혹은 같은색의 일반블럭이면 / 무지개 블럭이 탐색의 시작점이면 i,j=무지개 블럭의 좌표이므로 graph[i][j]=0이고 if문의 두 조건이 같은 것이된다.    
-                    if graph[nx][ny]==0 or graph[nx][ny]==graph[i][j]:
-                        q.append([nx,ny])
-                        visited[nx][ny]=1
-                        total_cnt+=1
-                        coordinate.append([nx,ny])
-                        #만약 무지개 블럭이면
-                        if graph[nx][ny]==0:
-                            rainbow+=1
-                            rainbow_lst.append([nx,ny])
-                        #무지개 블럭이 아니고, 일반 블럭이면
-                        if graph[nx][ny]!=0:
-                            if nx<gizoon_x and ny<gizoon_y:
-                                gizoon_x,gizoon_y = nx,ny
-    
-    for i in rainbow_lst:
-        visited[i[0]][i[1]]=0
-
-    if total_cnt>=2 and total_cnt-rainbow>0:
-        return total_cnt, rainbow, gizoon_x, gizoon_y, coordinate
-
-
-def gravity_bfs(x,y):
-    q=deque()
-    q.append([x,y])
-
-    while q:
-        x,y=q.popleft()
-        nx=x+1
-        if 0<=nx<n and graph[nx][y]==-2:   #graph[nx][y]!=-1 and 
-            q.append([nx,y])
-    
-    return x,y
-
-
-
-#중력작용(검은색 블록 제외 아래로 이동)
-def gravity():
-    for i in range(n-2,-1,-1):
-        for j in range(n):
-            if graph[i][j]!=-1 and graph[i][j]!=-2 :
-                x,y=gravity_bfs(i,j)
-                graph[x][y]=graph[i][j]
-                if (x,y)!=(i,j):
-                    graph[i][j]=-2
-
+#점수
 score=0
 
-#회전
-def turn():
-    turn_graph=[[0 for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            turn_graph[n-j-1][i]=graph[i][j]
-    return turn_graph
-
-
-
+#오토 플레이
 while 1:
-    visited=[[0 for _ in range(n)] for _ in range(n)]
-    block_group=[] #크기가 같은 블록그룹 모음 리스트
-    count=0
+    visited=[[0 for _ in range(n)] for _ in range(n)] #방문 체크
+    block_group=[] #블럭 그룹
+    count=0 #인접한 블럭 수
+
+    # bfs 탐색할때, i,j 가 기준블럭이 된다.(기준 블록은 무지개 블록이 아닌 블록 중에서 행의 번호가 가장 작은 블록, 그러한 블록이 여러개면 열의 번호가 가장 작은 블록이다.)
     for i in range(n):
         for j in range(n):
-            if visited[i][j]==0 and graph[i][j]!=-1 and graph[i][j]!=-2 and graph[i][j]!=0: #무지객 블록부터 시작하는 경우를 제외한다.(왜냐면 62번째줄을 보자)
-                res=bfs(i,j)
-                if res and count<=res[0]:
+            #방문하지 않았고, 일반 블록일 경우(무지개 블록이 시작하는 경우를 제외) ->  무지개 블럭 부터 탐색할 경우 bfs 에서 같은 색의 블럭을 찾을때 무지개 블럭만 찾게됨.
+            if visited[i][j]==0 and graph[i][j]>0:
+                res=bfs(i,j,graph[i][j]) # res= (총 갯수, 무지개 갯수, 해당 블럭들의 좌표)
+                
+                # res 가 None 이 아니고, 전체 블럭의 수가 count 보다 크고 2보다 크거나 같은 경우만 필터링한다.
+                if res and count<=res[0] and res[0]>=2:
                     block_group.append(res)
                     count=res[0]
     
+    #bfs 탐색을 했는데, 가능한 블럭 그룹이 없으면 종료.
     if len(block_group)==0:
         print(score)
         exit()
     
+    # 내림차순으로 정렬
     ans=sorted(block_group, key=lambda x : (-x[0], -x[1], -x[2],-x[3]))
+    #정렬 결과중 맨 앞의 것이 조건에 부합하는 블럭 그룹
     ans=ans[0]
     score+=count**2
+
     # 좌표 빈칸 처리 (빈칸을 -2로 처리)
-    for i in ans[4]:
-        graph[i[0]][i[1]]=-2
-    
+    for x,y in ans[4]:
+        graph[x][y]=-2
+
     #중력작용
     gravity()
 
