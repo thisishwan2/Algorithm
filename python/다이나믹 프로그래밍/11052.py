d3c3e374426b69784944bfe7351b93fdcdb4afe2
Hawn <feel000617@gmail.com>
2023-03-14 23:55:27 +0900

    dp

--- python/다이나믹 프로그래밍/11052.py
+++ python/다이나믹 프로그래밍/11052.py
@@ -3,8 +3,8 @@
 n=int(input())
 cardpack=[0]+list(map(int, input().split()))
 dp=[0]*(n+1)
-
+#dp: i개 삿을때의 최대 지불 금액
 for i in range(1, n+1):
     for j in range(1,i+1):
         dp[i]=max(dp[i], dp[i-j]+cardpack[j]) #dp[i-j]: i-j개의 카드를 구매할때 최대 가격, cardpack[j]: j개 카드 가격 (i-j)+(j)=i
-print(dp)
+print(max(dp))
