13698b245cd242e0e11a00fa69dd52d452d41afc
thisishwan2 <feel000617@gmail.com>
2023-03-30 10:49:21 +0900

    6497

---
+++ python/최소신장트리/1647.py
@@ -0,0 +1,38 @@
+#1647
+
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+edges = []
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+
+parent = [i for i in range(n+1)]
+total_cost = 0
+max_cost = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        total_cost += cost
+        max_cost = max(max_cost, cost)
+
+print(total_cost - max_cost)
