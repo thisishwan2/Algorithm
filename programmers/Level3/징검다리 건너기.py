a55faf0a672cc5dc352e21aa253d6a266660fd17
thisishwan2 <feel000617@gmail.com>
2024-05-29 17:31:14 +0900

    슬라이딩 윈도우

--- programmers/Level3/징검다리 건너기.py
+++ programmers/Level3/징검다리 건너기.py
@@ -1,4 +1,42 @@
-def solution(stones, k):
+from collections import deque
+
+from collections import deque
+
+
+def solution_sliding_window(stones, k):
+    n = len(stones)
+    q = deque()
+    q.append(0)  # 0번째 인덱스 추가
+
+    for i in range(1, k):
+
+        # 집어넣는 값보다 이미 큐의 마지막에 존재하는 값이 작은 경우에는 앞쪽의 값들을 없앤다.
+        while q and stones[q[-1]] < stones[i]:
+            q.pop()
+
+        # 비었거나, 값이 더 큰 경우에만 집어넣기
+        if not q or stones[q[-1]] >= stones[i]:
+            q.append(i)
+
+    answer = stones[q[0]]
+
+    for i in range(k, n):
+        # 큐의 가장 마지막 부분을 제거
+        while q and stones[q[-1]] < stones[i]:
+            q.pop()
+
+        # 앞쪽 제거
+        while q and q[0] <= i - k:
+            q.popleft()
+
+        # 더 큰 경우에만 집어넣기
+        if not q or stones[q[-1]] >= stones[i]:
+            q.append(i)
+
+        answer = min(answer, stones[q[0]])
+    return answer
+
+def solution_bs(stones, k):
     answer = 0
 
     # 이진탐색(배열의 크기가 2억이므로)
@@ -24,6 +62,8 @@ def solution(stones, k):
             start = mid + 1
 
     return answer
+
+
 
 # 효율성 실패
 # def solution(stones, k):
